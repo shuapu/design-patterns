5dfd7797f6cc201c0b8032c52ffab254fc40329b
shuapu <apurvshukla99@gmail.com>
2025-08-16 14:51:10 +0530

    Added Singleton design patten with : 1. Thread Safety 2. Using inner class.

---
+++ src/Creational/SingletonBillPugh.java
@@ -0,0 +1,21 @@
+package Creational;
+
+public class SingletonBillPugh {
+    /*
+      Pros : Best approach..Lazy init and no synchronization overhead.
+      Cons : No
+     */
+    private SingletonBillPugh() {
+
+    }
+
+    public static class Helper {
+        public static SingletonBillPugh INSTANCE = new SingletonBillPugh();
+    }
+
+    public SingletonBillPugh getInstance() {
+        return Helper.INSTANCE;
+    }
+
+
+}
--- src/Creational/SingletonLazy.java
+++ src/Creational/SingletonLazy.java
@@ -1,22 +1,53 @@
 package Creational;
 
 public class SingletonLazy {
-    /*
-    Pros : Simple,Memory optimised(Object created only when used)
-    Cons : Not a thread safe implementation.
-     */
 
     private static SingletonLazy instance = null;
 
+    /**
+     * Private Constructor to avoid creation of object outside class.
+     */
     private SingletonLazy() {
 
     }
 
+    /**
+     * This is not thread safe but Lazy initialization
+     *
+     * @return SingletonLazy
+     */
     public static SingletonLazy getInstance() {
         if (instance == null) {
             instance = new SingletonLazy();
         }
         return instance;
     }
+
+    /**
+     * Thread Safe and Lazy initialization but comparatively slow as complete method is synchronized.
+     *
+     * @return SingletonLazy
+     */
+    public static synchronized SingletonLazy getThreadSafeInstance() {
+        if (instance == null) {
+            instance = new SingletonLazy();
+        }
+        return instance;
+    }
+
+    /**
+     * Thread Safe and Lazy initialization but performance optimised.
+     *
+     * @return SingletonLazy
+     */
+    public static SingletonLazy getBlockThreadSafeInstance() {
+        if (instance == null) {
+            synchronized (SingletonLazy.class) {
+                instance = new SingletonLazy();
+            }
+        }
+        return instance;
+    }
+
 
 }
