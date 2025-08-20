6eba6195604841ae725a1864ae6c817d7f57609c
shuapu <apurvshukla99@gmail.com>
2025-08-20 14:03:01 +0530

    Added Details in strategy patterns.

--- src/Behavioural/Strategy/MainClass.java
+++ src/Behavioural/Strategy/MainClass.java
@@ -2,16 +2,24 @@ package Behavioural.Strategy;
 
 public class MainClass {
     public static void main(String[] args) {
-        EconomyDriveMode economyDriveMode = new EconomyDriveMode();
-        SportsDrivingMode sportsDrivingMode = new SportsDrivingMode();
-        PowerDrivingMode powerDrivingMode = new PowerDrivingMode();
+        /*
+        Use case : When a class have n  number of children and out of n , m has simpler behaviour .
+        Then we should use strategy patterns to avoid code duplicate in all m children.
+         */
+
+        DriveMode economyDriveMode = new EconomyDriveMode();
+        DriveMode sportsDrivingMode = new SportsDrivingMode();
+        DriveMode powerDrivingMode = new PowerDrivingMode();
 
         PassengerVehicle passengerVehicle = new PassengerVehicle(economyDriveMode);
         passengerVehicle.drive();
+
         SportsVehicle sportsVehicle = new SportsVehicle(sportsDrivingMode);
         sportsVehicle.drive();
+
         CommercialVehicle commercialVehicle = new CommercialVehicle(powerDrivingMode);
         commercialVehicle.drive();
+
         OffRodeVehicle offRodeVehicle = new OffRodeVehicle(powerDrivingMode);//Strategy created used in two different children.
         offRodeVehicle.drive();
 
